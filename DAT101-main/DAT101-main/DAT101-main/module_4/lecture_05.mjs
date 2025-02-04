3bc814f1f27f7a52110c50c501abc0e5e5d90dde
Taigah <clauveforlandu@gmail.com>
2025-02-04 15:21:13 +0100

    04.02 del 3

--- DAT101-main/DAT101-main/DAT101-main/module_4/lecture_05.mjs
+++
@@ -1,9 +0,0 @@
-import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
-initPrintOut(document.getElementById("txtOut"));
-
-const girlsName = ["Alice", "Betty", "Cathy", "Diana", "Eve", "Fiona", "Gina", "Helen", "Irene", "Jane"];
-
-const newGirlsName = girlsName.splice(1,4);
-printOut(girlsName.join(", "));
-printOut("");
-printOut(newGirlsName.join(", "));
